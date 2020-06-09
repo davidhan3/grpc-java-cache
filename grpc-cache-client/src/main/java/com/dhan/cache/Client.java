8535bd63662a52535526def63f3ab8a8c80f74fc
David Han <davidhan1619@gmail.com>
2020-06-09 16:49:22 -0700

    Changing client to use environment var but defaults to localhost:8080

--- grpc-cache-client/src/main/java/com/dhan/cache/Client.java
+++ grpc-cache-client/src/main/java/com/dhan/cache/Client.java
@@ -8,13 +8,15 @@ public class Client
 {
     public static void main( String[] args ) throws Exception
     {
+        String serverUrl = System.getenv("GRPC_SERVER_URL") != null ? System.getenv("GRPC_SERVER_URL") : "localhost:8080";
+
         Command command = CommandOperation.getOperation(args);
         if(command == Command.INVALID){
             System.out.println("Invalid command received");
             System.exit(1);
         }
         // Channel is the abstraction to connect to a service endpoint, plaintext because no SSL
-        final ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080").usePlaintext().build();
+        final ManagedChannel channel = ManagedChannelBuilder.forTarget(serverUrl).usePlaintext().build();
         CacheServiceGrpc.CacheServiceBlockingStub stub = CacheServiceGrpc.newBlockingStub(channel);
         CacheClient cache = new CacheClient(channel, stub);
 
